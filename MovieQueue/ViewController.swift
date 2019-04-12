f9b1dff89587e7ccd86af196acc683408cc7648c
Susan Zheng <Zheng.susan12@gmail.com>
2019-04-11 21:16:50 -0400

    Initial Commit

---
+++ MovieQueue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieQueue
+//
+//  Created by Susan Zheng on 4/11/19.
+//  Copyright © 2019 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
